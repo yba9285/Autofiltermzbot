dfb5fe4883f90bb860a6dc567fbe9fd6e3240d3b
yba9285 <108516941+yba9285@users.noreply.github.com>
2025-07-26 11:20:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -79,15 +79,15 @@ CAPTION_LANGUAGES = ["Bhojpuri", "Hindi", "Bengali", "Tamil", "English", "Bangla
 VERIFY = bool(environ.get('VERIFY', True))  # Verification On (True) / Off (False)
 DEENDAYAL_VERIFY_EXPIRE = int(environ.get('DEENDAYAL_VERIFY_EXPIRE', 24))  # Add time in hours
 DEENDAYAL_VERIFIED_LOG = int(environ.get('DEENDAYAL_VERIFIED_LOG', '-1002606471643'))  # Log channel id (make sure bot is admin)
-HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/how_to_open_mdiskshortner/9')  # How to open tutorial link for verification
+HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/how_to_open_mdiskshortner/10')  # How to open tutorial link for verification
 
 # ============================
 # Link Shortener Configuration
 # ============================
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'bharatlinks.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '0dc54d0cad495985f8e2c9bf1c6c82896bb537c7')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/how_to_open_mdiskshortner/9')  # Tutorial video link for opening shortlink website
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'inshorturl.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'f0b67e13bda5380f9b4d252b3c82092ec456966e')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/how_to_open_mdiskshortner/10')  # Tutorial video link for opening shortlink website
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 
 # ============================
